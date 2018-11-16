41d40b8ea7298999d7c4886c0aa3b785763ee28c
= <jsangiamo@wustl.edu>
2018-11-15 21:33:59 -0600

    created functional user registration route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+// loading usermodel
+const User = require("../../models/User");
 
 // all routes already start with /api/users
 
@@ -7,5 +12,49 @@ const router = express.Router();
 // @desc    Tests user route
 // @access  public
 router.get("/test", (req, res) => res.json({ msg: "Users API works" }));
+
+// @route   GET api/users/register
+// @desc    Register a new user
+// @access  public
+router.post("/register", (req, res) => {
+  // first see if email is already in DB
+  User.findOne({ username: req.body.username }).then(user => {
+    //check if user exists
+    if (user) {
+      return res.status(400).json({
+        useranme: "Username already exists"
+      });
+    }
+    // this email is not taken
+    else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //size of the image
+        r: "pg", //rating of the image (for preventing nudity, etc.)
+        d: "mm" // default image if user does not have a gravatar
+      });
+
+      const newUser = new User({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        avatar: avatar,
+        password: req.body.password
+      });
+
+      // generating hash for password
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 // bringing in routes
 const users = require("./routes/api/users");
 const profiles = require("./routes/api/profile");
 
 const app = express();
+
+// body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB config
 const db = require("./config/keys").mongoURI;
