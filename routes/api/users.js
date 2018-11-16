c9d4ccb226df2fefd6cb2ba3a3f41c44ebb06c2b
= <jsangiamo@wustl.edu>
2018-11-15 21:03:46 -0600

    annoted dummy routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 // all routes already start with /api/users
+
+// @route   GET api/users/test
+// @desc    Tests user route
+// @access  public
 router.get("/test", (req, res) => res.json({ msg: "Users API works" }));
 
 module.exports = router;
