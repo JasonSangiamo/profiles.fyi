5df6c88c4674197d90e205e76e2aadafea16d634
= <jsangiamo@wustl.edu>
2018-11-15 21:04:04 -0600

    annoted dummy routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 // all routes already start with /api/profile
+
+// @route   GET api/profile/test
+// @desc    Tests profile route
+// @access  public
 router.get("/test", (req, res) => res.json({ msg: "Profile API works" }));
 
 module.exports = router;
