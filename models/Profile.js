5faa1e1a8e468651eb2e6e4972e18933bfb9d1ff
= <jsangiamo@wustl.edu>
2018-11-18 16:42:05 -0600

    added route to get current user's profile

---
+++ models/Profile.js
@@ -0,0 +1,123 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema for profile
+const ProfileSchema = new Schema({
+  // associating user and profile
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  // slug for url
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  // status will be selected from a drop down (student, junior dev, etc...)
+  status: {
+    type: String,
+    required: true
+  },
+  // array of skills for each user
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String,
+    required: true
+  },
+  githubUseranme: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      // to date not required because jobmay be ongoing
+      to: {
+        type: Date,
+        required: true
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String,
+        required: false
+      }
+    }
+  ],
+
+  educaton: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldOfStudy: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      // to date not required because jobmay be ongoing
+      to: {
+        type: Date,
+        required: true
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String,
+        required: false
+      }
+    }
+  ],
+  linkedin: {
+    type: String
+  },
+  data: {
+    type: Date,
+    defualt: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Loading profile and user models
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 // all routes already start with /api/profile
 
-// @route   GET api/profile/test
-// @desc    Tests profile route
-// @access  public
-router.get("/test", (req, res) => res.json({ msg: "Profile API works" }));
+// @route   GET api/profile
+// @desc    Get current user's profile
+// @access  private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    // get current user
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noProfile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(errors));
+  }
+);
 
 module.exports = router;
