385e0db855e0feddede975376af9476fd00198ab
= <jsangiamo@wustl.edu>
2018-11-23 17:51:36 -0500

    created front end for add experience form

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import Register from "./components/auth/Register";
 import Dashboard from "./components/dasboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
+import AddExperience from "./components/add-credentials/AddExperience";
 import { clearCurrentProfile } from "./actions/profileActions";
 
 //check for token
@@ -77,6 +78,14 @@ class App extends Component {
                   exact
                   path="/edit-profile"
                   component={EditProfile}
+                />
+              </Switch>
+
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
                 />
               </Switch>
             </div>
